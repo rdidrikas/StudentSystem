db60bf0626500ad26f3c98587ae06ba47d0f313f
Rokas <rdidrikas@gmail.com>
2025-04-30 12:44:33 +0300

    Data loads from students.csv when launched

--- src/main/java/rdid/studentssys/Main.java
+++ src/main/java/rdid/studentssys/Main.java
@@ -5,14 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import rdid.studentssys.data.CSVhandler;
 
 import java.io.IOException;
 import java.util.Objects;
 
+// Rokas Danielius Didrikas Gr. 5
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
+
+        CSVhandler csvHandler = new CSVhandler();
+        csvHandler.studentCSVData(csvHandler.importData("src/main/resources/data/students.csv")); // loading data
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/rdid/studentssys/home.fxml")));
 
         // Create the scene
